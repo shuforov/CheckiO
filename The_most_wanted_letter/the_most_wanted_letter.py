9b23a7371eb9c98522b0c7591b40e69271367512
shuforov <shuforov@gmail.com>
2016-02-10 20:55:29 +0200

    0.1 in progres

--- The_most_wanted_letter/the_most_wanted_letter.py
+++ The_most_wanted_letter/the_most_wanted_letter.py
@@ -2,6 +2,7 @@ import string
 diction_l = string.ascii_lowercase
 def checkio(string):
 	count_l = {}
+	new_count = []
 	z = string.encode('ascii','replace')
 	new_string = str.lower(z)
 	for y in diction_l:
@@ -9,7 +10,20 @@ def checkio(string):
 	for x in new_string:
 		if x in diction_l:
 			count_l[x] +=1
-	print sorted(count_l.values())
+	for h in count_l:
+		if count_l[h] > 0:
+			new_count.append((h,count_l[h]))
+	print new_count
+	for aa in new_count:
+		print aa
+		if aa[1] > 2:
+			print aa[0]
+		
+	# print new_count
+	# t = sorted(count_l.items())
+	# print t.sort()
+	# j = sorted(new_count.values())
+	# print j[-1:]
 
     #These "asserts" using only for self-checking and not necessary for auto-testing
 print checkio(u"Hello World!") 
