8d657ea5a747a2421c043190fb362d84ba4446ec
shuforov <shuforov@gmail.com>
2016-02-11 18:51:36 +0200

    0.2 in progres

--- The_most_wanted_letter/the_most_wanted_letter.py
+++ The_most_wanted_letter/the_most_wanted_letter.py
@@ -1,37 +1,28 @@
 import string
 diction_l = string.ascii_lowercase
+def getKey(item):
+	return item[1]
+
 def checkio(string):
 	count_l = {}
-	new_count = []
+	converted_list_from_count_l = []
 	z = string.encode('ascii','replace')
 	new_string = str.lower(z)
-	for y in diction_l:
-		count_l[y]= 0
-	for x in new_string:
-		if x in diction_l:
-			count_l[x] +=1
-	for h in count_l:
-		if count_l[h] > 0:
-			new_count.append((h,count_l[h]))
-	print new_count
-	for aa in new_count:
-		print aa
-		if aa[1] > 2:
-			print aa[0]
-		
-	# print new_count
-	# t = sorted(count_l.items())
-	# print t.sort()
-	# j = sorted(new_count.values())
-	# print j[-1:]
+	for le in new_string:
+		if le in diction_l:
+			count_l[le] = 0
+	for le_update in new_string:
+		if le_update in diction_l:
+			count_l[le_update] += 1
+	for element in count_l:
+		converted_list_from_count_l.append((element, count_l[element]))
+	list_sort_by_key = sorted(converted_list_from_count_l, key=getKey, reverse=True)
+	list_sort_by_le = sorted(converted_list_from_count_l)
+	disp_le = list_sort_by_key[0]
+	print list_sort_by_key
+	print list_sort_by_le
+	return disp_le[0]
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-print checkio(u"Hello World!") 
-print checkio(u"How do you do?") 
-print checkio(u"One") 
-print checkio(u"Oops!") 
-print checkio(u"AAaooo!!!!") 
-print checkio(u"abe") 
-print("Start the long test")
-print checkio(u"a" * 9000 + u"b" * 1000) 
-print("The local tests are done.")
+#These "asserts" using only for self-checking and not necessary for auto-testing
+print checkio(u"fsbd")
+
