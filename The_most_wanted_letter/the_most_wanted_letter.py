c81ad3c845147533959679aa463ec9e19625ea45
shuforov <shuforov@gmail.com>
2016-02-11 20:32:27 +0200

    0.5

--- The_most_wanted_letter/the_most_wanted_letter.py
+++ The_most_wanted_letter/the_most_wanted_letter.py
@@ -1,9 +1,30 @@
+"""
+You are given a text, which contains different english letters and punctuation symbols. You should find the most frequent letter in the text. The letter returned must be in lower case.
+While checking for the most wanted letter, casing does not matter, so for the purpose of your search, "A" == "a". Make sure you do not count punctuation symbols, digits and whitespaces, only letters.
+
+If you have two or more letters with the same frequency, then return the letter which comes first in the latin alphabet. For example -- "one" contains "o", "n", "e" only once for each, thus we choose "e".
+
+Input: A text for analysis as a string (unicode for py2.7).
+
+Output: The most frequent letter in lower case as a string.
+
+Example:
+
+checkio("Hello World!") == "l"
+checkio("How do you do?") == "o"
+checkio("One") == "e"
+checkio("Oops!") == "o"
+checkio("AAaooo!!!!") == "a"
+checkio("abe") == "a"
+
+How it is used: For most decryption tasks you need to know the frequency of occurrence for various letters in a section of text. For example: we can easily crack a simple addition or substitution cipher if we know the frequency in which letters appear. This is interesting stuff for language experts!
+"""
 import string
 diction_l = string.ascii_lowercase # creating of 
 def getKey(item):
 	return item[1]
 
-def in_tapl(tap_o):
+def in_tapl(tap_o): 
 	first_tapl = tap_o[0]
 	return first_tapl[1]
 
@@ -28,20 +49,12 @@ def checkio(string):
 	maximum = max(list_sort_by_key)
 	max_num = maximum[1]
 	first_pice = in_tapl(list_sort_by_key)
-	most_wanted_letter_one = list_sort_by_key[0]
-	most_wanted_letter_two = list_sort_by_letters[0]
 
 	for j in list_sort_by_key:
 		if first_pice == j[1]:
 			conjuction +=1
-			print j
-	print first_pice
-	print 
-	print max_num
-	print 
-	print conjuction
+
 	# print list_sort_by_key
-	print list_sort_by_letters
 	
 	temp_list = []
 	
@@ -60,5 +73,12 @@ def checkio(string):
 	return temp_list_3[0]
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
-print checkio(u"Lorem ipsum dolor sit amet")
-
+print checkio(u"Hello World!")
+print checkio(u"How do you do?")
+print checkio(u"One")
+print checkio(u"Oops!")
+print checkio(u"AAaooo!!!!")
+print checkio(u"abe")
+print("Start the long test")
+print checkio(u"a" * 9000 + u"b" * 1000)
+print("The local tests are done.")
