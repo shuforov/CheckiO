af67308a21742b22c4ec1a921a9683a2e5a8507b
shuforov <shuforov@gmail.com>
2016-02-11 20:24:33 +0200

    0.4

--- The_most_wanted_letter/the_most_wanted_letter.py
+++ The_most_wanted_letter/the_most_wanted_letter.py
@@ -4,7 +4,7 @@ def getKey(item):
 	return item[1]
 
 def in_tapl(tap_o):
-	first_tapl = tap_o[1]
+	first_tapl = tap_o[0]
 	return first_tapl[1]
 
 def checkio(string):
@@ -25,18 +25,40 @@ def checkio(string):
 	length_of_tapl = len(converted_list_from_count_letters)
 	list_sort_by_key = sorted(converted_list_from_count_letters, key=getKey, reverse=True)
 	list_sort_by_letters = sorted(converted_list_from_count_letters)
-	most_wanted_letter = list_sort_by_key[0]
-	print
-	print length_of_tapl
-	print
-	print list_sort_by_key
+	maximum = max(list_sort_by_key)
+	max_num = maximum[1]
+	first_pice = in_tapl(list_sort_by_key)
+	most_wanted_letter_one = list_sort_by_key[0]
+	most_wanted_letter_two = list_sort_by_letters[0]
+
+	for j in list_sort_by_key:
+		if first_pice == j[1]:
+			conjuction +=1
+			print j
+	print first_pice
+	print 
+	print max_num
+	print 
+	print conjuction
+	# print list_sort_by_key
 	print list_sort_by_letters
+	
+	temp_list = []
+	
+	if conjuction == 1:
+		for t_l in list_sort_by_key:
+			if first_pice == t_l[1]:
+				temp_list.append(t_l)
+	elif conjuction >= 2:
+		for t_l in list_sort_by_key:
+			if first_pice == t_l[1]:
+				temp_list.append(t_l)
 
+	temp_list_2 = sorted(temp_list)
+	temp_list_3 = temp_list_2[0]
 
-	return most_wanted_letter[0]
-
-
+	return temp_list_3[0]
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
-print checkio(u"fsssbbbd")
+print checkio(u"Lorem ipsum dolor sit amet")
 
