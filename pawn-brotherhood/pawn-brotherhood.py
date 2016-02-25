cdf66634b4fd4b8ee3eef226a167f0f1010a29ed
shuforov <shuforov@gmail.com>
2016-02-25 15:25:23 +0200

    chass_start

---
+++ pawn-brotherhood/pawn-brotherhood.py
@@ -0,0 +1,12 @@
+##http://www.checkio.org/mission/pawn-brotherhood/solve/
+
+
+def safe_pawns(pawns):
+
+    return 0
+
+
+print safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})
+print safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"})
+##print safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6
+##print safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) == 1
