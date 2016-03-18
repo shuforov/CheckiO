658f7a1572928944ed20826db19e8371f8c9d494
shuforov <shuforov@gmail.com>
2016-03-18 11:47:15 +0200

    start_ip

--- IP_network_route_summarization/IP_network_route_summarization.py
+++ IP_network_route_summarization/IP_network_route_summarization.py
@@ -1,1 +1,10 @@
 # http://www.checkio.org/mission/ip-network-route-summarization/
+def checkio(data):
+    #replace this for solution
+    return ""
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+
+# (checkio(["172.16.12.0", "172.16.13.0", "172.16.14.0", "172.16.15.0"]) == "172.16.12.0/22"), "First Test"
+# (checkio(["172.16.12.0", "172.16.13.0", "172.155.43.9"]) == "172.0.0.0/8"), "Second Test"
+# (checkio(["172.16.12.0", "172.16.13.0", "172.155.43.9", "146.11.2.2"]) == "128.0.0.0/2"), "Third Test"
