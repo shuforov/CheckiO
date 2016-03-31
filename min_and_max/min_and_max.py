5fabf274e58fdeb367e19249d106fc11e0a59e77
shuforov <shuforov@gmail.com>
2016-03-31 21:40:15 +0300

    simple sort for any type

--- min_and_max/min_and_max.py
+++ min_and_max/min_and_max.py
@@ -1,15 +1,34 @@
-#/home/Dump/.git-credential-cache
+#http://www.checkio.org/mission/min-max/
+
 def min(*args, **kwargs):
     key = kwargs.get("key", None)
-    return None
+    print type(args)
+    print kwargs
 
+    z = 0
+    list_for_sort = []
+    string_v = ''
+    type_of_var = 0
+    for x in args:
+        if isinstance(x,list):
+            z = x
+            z = sorted(z)
+            type_of_var = 0
+        elif isinstance(x,int):
+            list_for_sort.append(x)
+            list_for_sort = sorted(list_for_sort)
+            type_of_var = 1
+        elif isinstance(x,str):
+            string_v += x
+            string_v = ''.join(sorted(string_v))
+            type_of_var = 2
+    list_return = [z,list_for_sort,string_v]
+    return list_return[type_of_var][0]
 
 def max(*args, **kwargs):
     key = kwargs.get("key", None)
-    return None
 
-print max(3, 2) == 3, "Simple case max"
-print min(3, 2) == 2, "Simple case min"
+print min([1,5,0,9])
 
 # print max(3, 2) == 3, "Simple case max"
 # print min(3, 2) == 2, "Simple case min"
