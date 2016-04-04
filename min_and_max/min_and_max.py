880bcda5dd7a9d79da88be1176af9eede71c60d4
shuforov <shuforov@gmail.com>
2016-04-04 18:41:19 +0300

    simple min max sort and sort with lambda lists

--- min_and_max/min_and_max.py
+++ min_and_max/min_and_max.py
@@ -4,31 +4,78 @@ def min(*args, **kwargs):
     key = kwargs.get("key", None)
     print type(args)
     print kwargs
+    if key == None:
+        z = 0
+        list_for_sort = []
+        string_v = ''
+        type_of_var = 0
+        for x in args:
+            if isinstance(x,list):
+                z = x
+                z = sorted(z)
+                type_of_var = 0
+            elif isinstance(x,int):
+                list_for_sort.append(x)
+                list_for_sort = sorted(list_for_sort)
+                type_of_var = 1
+            elif isinstance(x,str):
+                string_v += x
+                string_v = ''.join(sorted(string_v))
+                type_of_var = 2
+        list_return = [z,list_for_sort,string_v]
+        return list_return[type_of_var][0]
+    elif key != None:
+        if isinstance(args[0],list):
+            check_num = key(args[0][0])
+            counter = 0
+            the_min = 0
+            for z in args:
+                for x in z:
+                     if key(x) < check_num:
+                         check_num = key(x)
+                         the_min = counter
+                     counter += 1
+            return args[0][the_min]
 
-    z = 0
-    list_for_sort = []
-    string_v = ''
-    type_of_var = 0
-    for x in args:
-        if isinstance(x,list):
-            z = x
-            z = sorted(z)
-            type_of_var = 0
-        elif isinstance(x,int):
-            list_for_sort.append(x)
-            list_for_sort = sorted(list_for_sort)
-            type_of_var = 1
-        elif isinstance(x,str):
-            string_v += x
-            string_v = ''.join(sorted(string_v))
-            type_of_var = 2
-    list_return = [z,list_for_sort,string_v]
-    return list_return[type_of_var][0]
 
 def max(*args, **kwargs):
     key = kwargs.get("key", None)
+    print type(args)
+    print kwargs
+    if key == None:
+        z = 0
+        list_for_sort = []
+        string_v = ''
+        type_of_var = 0
+        for x in args:
+            if isinstance(x,list):
+                z = x
+                z = sorted(z)
+                type_of_var = 0
+            elif isinstance(x,int):
+                list_for_sort.append(x)
+                list_for_sort = sorted(list_for_sort)
+                type_of_var = 1
+            elif isinstance(x,str):
+                string_v += x
+                string_v = ''.join(sorted(string_v))
+                type_of_var = 2
+        list_return = [z,list_for_sort,string_v]
+        return list_return[type_of_var][len(list_return[type_of_var]) - 1]
+    elif key != None:
+        if isinstance(args[0],list):
+            check_num = key(args[0][0])
+            counter = 0
+            the_max = 0
+            for z in args:
+                for x in z:
+                     if key(x) > check_num:
+                         check_num = key(x)
+                         the_max = counter
+                     counter += 1
+            return args[0][the_max]
 
-print min([1,5,0,9])
+print max(4,1,5,6,2)
 
 # print max(3, 2) == 3, "Simple case max"
 # print min(3, 2) == 2, "Simple case min"
