ac9d342ff62898466a93787515b081142da5b54f
shuforov <shuforov@gmail.com>
2016-04-05 12:59:12 +0300

    start it

---
+++ the_most_frequent_weekdays/the_most_frequent_weekdays.py
@@ -0,0 +1,14 @@
+#https://checkio.org/mission/the-most-frequent-weekdays/
+
+def most_frequent_days(year):
+    """
+        List of most frequent days of the week in the given year
+    """
+    return ['Monday']
+
+print most_frequent_days(2399)
+
+    # assert most_frequent_days(2399) ==  ['Friday'], "1st example"
+    # assert most_frequent_days(1152) == ['Tuesday', 'Wednesday'], "2nd example"
+    # assert most_frequent_days(56) == ['Saturday', 'Sunday'], "3rd example"
+    # assert most_frequent_days(2909) == ['Tuesday'], "4th example"
