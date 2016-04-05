c865bab0d71109462456ef359f2d5c05b1d801a5
shuforov <shuforov@gmail.com>
2016-04-05 20:26:55 +0300

    need to think

--- the_most_frequent_weekdays/the_most_frequent_weekdays.py
+++ the_most_frequent_weekdays/the_most_frequent_weekdays.py
@@ -1,11 +1,22 @@
 #https://checkio.org/mission/the-most-frequent-weekdays/
-
+import calendar
+z = calendar.Calendar(calendar.MONDAY)
+v = calendar.TextCalendar(firstweekday=0)
 def most_frequent_days(year):
-    """
-        List of most frequent days of the week in the given year
-    """
-    return ['Monday']
-
+    month = 1
+    print calendar.monthrange(year, month)
+    list_temp = []
+    for x in calendar.day_name:
+        list_temp.append(x)
+    print list_temp
+    g = z.iterweekdays()
+    list_for_days = []
+    for x in g:
+        list_for_days.append(x)
+    print list_for_days
+    t = z.itermonthdays(year, month)
+    print v.formatmonth(year,month,w=0,l=0)
+    print calendar.MONDAY
 print most_frequent_days(2399)
 
     # assert most_frequent_days(2399) ==  ['Friday'], "1st example"
