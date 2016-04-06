042eb5966d11a7d537dd387a9d2bee5d2f94b63a
shuforov <shuforov@gmail.com>
2016-04-06 10:45:12 +0300

    create:list with name of the week, dictionary in number type days, list with number of days month, simple calendar

--- the_most_frequent_weekdays/the_most_frequent_weekdays.py
+++ the_most_frequent_weekdays/the_most_frequent_weekdays.py
@@ -2,21 +2,40 @@
 import calendar
 z = calendar.Calendar(calendar.MONDAY)
 v = calendar.TextCalendar(firstweekday=0)
+
 def most_frequent_days(year):
-    month = 1
-    print calendar.monthrange(year, month)
-    list_temp = []
-    for x in calendar.day_name:
-        list_temp.append(x)
-    print list_temp
-    g = z.iterweekdays()
-    list_for_days = []
-    for x in g:
-        list_for_days.append(x)
-    print list_for_days
-    t = z.itermonthdays(year, month)
-    print v.formatmonth(year,month,w=0,l=0)
-    print calendar.MONDAY
+    month = range(1,13)
+    for month_num in month:
+        #list days name of the week
+        list_temp = []
+        for x in calendar.day_name:
+            list_temp.append(x)
+        print list_temp
+        #creating dictionary for counting days
+        day_of_the_week = {}
+        day_number = range(0,8)
+        input_dict_counter = 0
+        for x in day_number:
+            day_of_the_week[x] = 0
+            input_dict_counter += 1
+        print day_of_the_week
+        #creating list with all days in month
+        gg = calendar.monthcalendar(year, month_num)
+        list_of_wholl_month = []
+        for x in gg:
+            list_of_wholl_month.append(x)
+        print list_of_wholl_month
+        #matching of days and count it
+        for x in list_of_wholl_month:
+            counter = 0
+            for z in x:
+                if z > 1:
+                    day_of_the_week[counter] += 1
+                counter += 1
+        #simple output calendar
+        print v.formatmonth(year,month_num,w=0,l=0)
+
+
 print most_frequent_days(2399)
 
     # assert most_frequent_days(2399) ==  ['Friday'], "1st example"
