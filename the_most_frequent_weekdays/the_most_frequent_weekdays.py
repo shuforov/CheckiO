da33c532ccf56f0f3f0b0ace68663e4be1511592
shuforov <shuforov@gmail.com>
2016-04-06 11:18:55 +0300

    return the one day, need fix to out the couple of days

--- the_most_frequent_weekdays/the_most_frequent_weekdays.py
+++ the_most_frequent_weekdays/the_most_frequent_weekdays.py
@@ -5,35 +5,45 @@ v = calendar.TextCalendar(firstweekday=0)
 
 def most_frequent_days(year):
     month = range(1,13)
+    # creating dictionary for counting days
+    day_of_the_week = {}
+    day_number = range(0, 7)
+    input_dict_counter = 0
+    for x in day_number:
+        day_of_the_week[x] = 0
+        input_dict_counter += 1
+    #perform each month
     for month_num in month:
         #list days name of the week
         list_temp = []
         for x in calendar.day_name:
             list_temp.append(x)
         print list_temp
-        #creating dictionary for counting days
-        day_of_the_week = {}
-        day_number = range(0,8)
-        input_dict_counter = 0
-        for x in day_number:
-            day_of_the_week[x] = 0
-            input_dict_counter += 1
-        print day_of_the_week
         #creating list with all days in month
         gg = calendar.monthcalendar(year, month_num)
         list_of_wholl_month = []
         for x in gg:
             list_of_wholl_month.append(x)
-        print list_of_wholl_month
         #matching of days and count it
         for x in list_of_wholl_month:
             counter = 0
             for z in x:
-                if z > 1:
+                if z >= 1:
                     day_of_the_week[counter] += 1
                 counter += 1
+        print day_of_the_week
         #simple output calendar
         print v.formatmonth(year,month_num,w=0,l=0)
+        #sorting the most frequent day and return it
+        match = day_of_the_week[0]
+        counter_for_match = 0
+        bigest_counter = 0
+        for x in day_of_the_week:
+            if day_of_the_week[x] > match:
+                match = day_of_the_week[x]
+                bigest_counter = counter_for_match
+            counter_for_match += 1
+        return list_temp[bigest_counter]
 
 
 print most_frequent_days(2399)
